c4aaeca70b4a2c44fc57de77d1cabfcfe441b21d
Matias Vainio <matiasva@metropolia.fi>
2020-10-24 00:05:49 +0300

    unicafe step2 done

--- osa_1/unicafe/src/index.js
+++ osa_1/unicafe/src/index.js
@@ -5,19 +5,50 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [avg, setAvg] = useState(0);
+
+  const increaseByOne = (props) => {
+    if (props === "good") {
+      setGood(good + 1);
+      setAvg(avg + 1);
+    } else if (props === "neutral") {
+      setNeutral(neutral + 1);
+    } else if (props === "bad") {
+      setBad(bad + 1);
+      setAvg(avg - 1);
+    }
+    setAll(all + 1);
+  };
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button onClick={() => setGood(good + 1)} text="Good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+      <Button onClick={() => increaseByOne("good")} text="Good" />
+      <Button onClick={() => increaseByOne("neutral")} text="Neutral" />
+      <Button onClick={() => increaseByOne("bad")} text="Bad" />
       <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+        <br />
+        all {all}
+        <br />
+        <CountAvg good={good} bad={bad} all={all} />
+      </p>
     </div>
   );
+};
+
+const CountAvg = ({ good, bad, all }) => {
+  if (all === 0) {
+    return <div>average 0</div>;
+  } else {
+    return <div>average {(good - bad) / all}</div>;
+  }
 };
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
