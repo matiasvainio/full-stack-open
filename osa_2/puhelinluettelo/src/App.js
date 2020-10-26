48e804d787bad75a3f722628d8fe75b4eb7709e6
matiasva <matias.vainio@metropolia.fi>
2020-10-26 18:33:17 +0200

    puhelinluettelo step3 done

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", number: "050 1234568" },
+    ]);
     const [newName, setNewName] = useState("");
+    const [number, setNumber] = useState("");
 
     const addName = (event) => {
         event.preventDefault();
         const personObject = {
             name: newName,
+            number: number,
         };
 
         const alreadyOnList = persons.find((o) => o.name === newName);
@@ -16,14 +20,18 @@ const App = () => {
         if (alreadyOnList === undefined) {
             setPersons(persons.concat(personObject));
             setNewName("");
+            setNumber("");
         } else {
             alert(`${newName} is already on the list`);
         }
     };
 
     const handlePersonChange = (event) => {
-        // console.log(event.target.value);
         setNewName(event.target.value);
+    };
+
+    const handleNumberChange = (event) => {
+        setNumber(event.target.value);
     };
 
     return (
@@ -31,8 +39,14 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={addName}>
                 <div>
-                    name:{" "}
-                    <input value={newName} onChange={handlePersonChange} />
+                    <div>
+                        name:
+                        <input value={newName} onChange={handlePersonChange} />
+                    </div>
+                    <div>
+                        number:
+                        <input value={number} onChange={handleNumberChange} />
+                    </div>
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -41,7 +55,11 @@ const App = () => {
             <h2>Numbers</h2>
             <ul>
                 {persons.map((person, i) => {
-                    return <li key={i}>{person.name}</li>;
+                    return (
+                        <li key={i}>
+                            {person.name} {person.number}
+                        </li>
+                    );
                 })}
             </ul>
         </div>
