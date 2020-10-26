d8cdd9db29548b194f64ca2d7bef623d77227f1c
matiasva <matias.vainio@metropolia.fi>
2020-10-26 16:35:17 +0200

    kurssitiedot step 9 done

--- osa_2/kurssitiedot/src/index.js
+++ osa_2/kurssitiedot/src/index.js
@@ -2,60 +2,83 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-            },
-        ],
-    };
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
     return (
         <>
-            <Course course={course} />
+            <Course course={courses} />
         </>
     );
 };
 
-const Course = ({ course }) => {
+const Course = (courses) => {
+    const course = courses.course;
+    console.log(course);
+    return course.map((course) => {
+        return (
+            <div key={course.id}>
+                <Header course={course.name} />
+                <Content parts={course.parts} />
+                <Total parts={course.parts} />
+            </div>
+        );
+    });
+};
+
+const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+    console.log(parts);
     return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+        <div key={parts.id}>
+            {parts.map((part) => (
+                <Part part={part.name} exercises={part.exercises} />
+            ))}
         </div>
-    );
-};
-
-const Header = (props) => {
-    return <h1>{props.course}</h1>;
-};
-
-const Content = (props) => {
-    return (
-        <>
-            <Part
-                part={props.parts[0].name}
-                exercises={props.parts[0].exercises}
-            />
-            <Part
-                part={props.parts[1].name}
-                exercises={props.parts[1].exercises}
-            />
-            <Part
-                part={props.parts[2].name}
-                exercises={props.parts[2].exercises}
-            />
-        </>
     );
 };
 
@@ -69,11 +92,11 @@ const Total = ({ parts }) => {
 
 const Part = (props) => {
     return (
-        <>
+        <div>
             <p>
                 {props.part} {props.exercises}
             </p>
-        </>
+        </div>
     );
 };
 
