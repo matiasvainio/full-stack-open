d452c093218e3decef2a00b923e3a485953b2dc9
matiasva <matias.vainio@metropolia.fi>
2020-10-27 13:41:08 +0200

    maiden tiedot step1 done

---
+++ osa_2/maiden-tiedot/src/App.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+            setCountries(response.data);
+        });
+    }, []);
+
+    const handleSearch = (event) => {
+        setCountries(filterCountries(countries, event.target.value));
+    };
+
+    const filterCountries = (countries, c) => {
+        return countries.filter((country) => country.name.includes(c));
+    };
+
+    console.log(countries);
+
+    return (
+        <div>
+            <div>
+                find countries
+                <input onChange={handleSearch} />
+                <Countries countries={countries} />
+            </div>
+        </div>
+    );
+}
+
+const Countries = (props) => {
+    const countries = props.countries;
+    if (countries.length > 10) {
+        return <p>Too many matches, specify another filter.</p>;
+    } else if (countries.length === 1) {
+        return <Country country={countries[0]} />;
+    }
+    return (
+        <ul>
+            {countries.map((c, i) => {
+                return <li key={i}>{c.name}</li>;
+            })}
+        </ul>
+    );
+};
+
+const Country = (props) => {
+    const country = props.country;
+    return (
+        <div>
+            <h1>{country.name}</h1>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h2>languages</h2>
+            <ul>
+                {country.languages.map((lan, i) => (
+                    <li key={i}>{lan.name}</li>
+                ))}
+            </ul>
+            <img alt="flag" src={country.flag} width="200" height="auto" />
+        </div>
+    );
+};
+
+export default App;
