3e6233e82bbee1d8123c22bee59f2846f9d88403
matiasva <matias.vainio@metropolia.fi>
2020-10-27 11:43:55 +0200

    puhelinluettelo step4 done

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -2,7 +2,10 @@ import React, { useState } from "react";
 
 const App = () => {
     const [persons, setPersons] = useState([
-        { name: "Arto Hellas", number: "050 1234568" },
+        { name: "Arto Hellas", number: "040-123456" },
+        { name: "Ada Lovelace", number: "39-44-5323523" },
+        { name: "Dan Abramov", number: "12-43-234345" },
+        { name: "Mary Poppendieck", number: "39-23-6423122" },
     ]);
     const [newName, setNewName] = useState("");
     const [number, setNumber] = useState("");
@@ -15,7 +18,6 @@ const App = () => {
         };
 
         const alreadyOnList = persons.find((o) => o.name === newName);
-        console.log(alreadyOnList);
 
         if (alreadyOnList === undefined) {
             setPersons(persons.concat(personObject));
@@ -34,11 +36,24 @@ const App = () => {
         setNumber(event.target.value);
     };
 
+    const handleFilter = (event) => {
+        setPersons(filterPersons(persons, event.target.value));
+    };
+
+    const filterPersons = (persons, f) => {
+        return persons.filter((person) => person.name.includes(f));
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <h2>Search</h2>
+            <div>
+                filter shown with: <input onChange={handleFilter} />
+            </div>
             <form onSubmit={addName}>
                 <div>
+                    <h2>Add a new</h2>
                     <div>
                         name:
                         <input value={newName} onChange={handlePersonChange} />
@@ -53,16 +68,23 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <ul>
-                {persons.map((person, i) => {
-                    return (
-                        <li key={i}>
-                            {person.name} {person.number}
-                        </li>
-                    );
-                })}
-            </ul>
+            <Person persons={persons} />
         </div>
+    );
+};
+
+const Person = (props) => {
+    const persons = props.persons;
+    return (
+        <ul>
+            {persons.map((person, i) => {
+                return (
+                    <li key={i}>
+                        {person.name} {person.number}
+                    </li>
+                );
+            })}
+        </ul>
     );
 };
 
