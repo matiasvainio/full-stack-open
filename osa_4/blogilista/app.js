8bcfd61e6a2cc3011b2e24ee60243e0052221f65
matiasva <matias.vainio@metropolia.fi>
2020-11-01 15:55:41 +0200

    blogilista steps 1 and 2 done

---
+++ osa_4/blogilista/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+const logger = require('./utils/logger');
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
