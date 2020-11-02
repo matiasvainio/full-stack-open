8cf60c043a4d501cca6a76ff8207580949763eac
matiasva <matias.vainio@metropolia.fi>
2020-11-02 14:47:21 +0200

    blogilistan testit, step2 done

--- osa_4/blogilista/models/blog.js
+++ osa_4/blogilista/models/blog.js
@@ -7,4 +7,17 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 });
 
+blogSchema.set('toJSON', {
+  transform: (doc, object) => {
+    const returnableObject = object;
+    // eslint-disable-next-line no-underscore-dangle
+    returnableObject.id = object._id;
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnableObject._id;
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnableObject.__v;
+    return returnableObject;
+  },
+});
+
 module.exports = mongoose.model('Blog', blogSchema);
--- osa_4/blogilista/tests/blog_api.test.js
+++ osa_4/blogilista/tests/blog_api.test.js
@@ -21,6 +21,14 @@ test('blogs are returned as json', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
+test('identfier field is called id', async () => {
+  const response = await api.get('/api/blogs').expect(200);
+  const mapped = response.body.map((id) => id.id);
+
+  console.log(mapped);
+  expect(mapped).toBeDefined();
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
