b75b5738b940ab331379e204476b537d2b5c2379
matiasva <matias.vainio@metropolia.fi>
2020-11-05 13:58:19 +0200

    blogilistan laajennus, step6 done

--- osa_4/blogilista/controllers/blogs.js
+++ osa_4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
@@ -11,12 +12,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map((blog) => blog.toJSON()));
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post('/', async (request, response) => {
   const { body } = request;
-
-  console.log(await User.find({}));
-
-  const user = await User.findById(body.userId);
+  const token = getTokenFrom(request);
+  console.log(token);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
@@ -40,6 +52,7 @@ blogsRouter.post('/', async (request, response) => {
     await user.save();
 
     response.status(201).json(savedBlog.toJSON());
+    return null;
   }
 });
 
