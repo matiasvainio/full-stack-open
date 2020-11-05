b0766efb27dde4dff5ea45060c9d7963ad772ee1
matiasva <matias.vainio@metropolia.fi>
2020-11-05 14:43:28 +0200

    blogilistan laajennus, step9 done

--- osa_4/blogilista/controllers/blogs.js
+++ osa_4/blogilista/controllers/blogs.js
@@ -15,7 +15,6 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { body } = request;
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  console.log(decodedToken);
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' });
   }
@@ -42,12 +41,11 @@ blogsRouter.post('/', async (request, response) => {
     user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
 
-    response.status(201).json(savedBlog.toJSON());
+    return response.status(201).json(savedBlog.toJSON());
   }
-  return null;
 });
 
-blogsRouter.delete('/:id', (request, response) => {
+blogsRouter.delete('/:id', async (request, response) => {
   // Blog.findByIdAndDelete(request.params.id, (err) => {
   //   if (err) {
   //     response.status(404).json();
@@ -55,9 +53,20 @@ blogsRouter.delete('/:id', (request, response) => {
   //     response.status(204).json();
   //   }
   // });
-  Blog.findById(request.params.id, () => {
-    console.log('delete', request.params.id);
-  });
+  const { body } = request;
+  const blog = await Blog.findById(request.params.id);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
+  if (blog.user.toString() === user.id.toString()) {
+    console.log('ids match');
+    response.status(204).end();
+  } else {
+    return response.status(401).json({ error: 'invalid token' });
+  }
 });
 
 blogsRouter.put('/:id', async (request, response) => {
--- osa_4/blogilista/utils/middleware.js
+++ osa_4/blogilista/utils/middleware.js
@@ -1,7 +1,6 @@
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
   if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    // request.json({ token: authorization.substring(7) });
     request.token = authorization.substring(7);
   }
   next();
