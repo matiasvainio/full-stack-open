25351b8e6e95a3063073dc62740c0a757a2f604a
Matias Vainio <matias.s.vainio@gmail.com>
2020-11-11 15:08:42 +0200

    working

--- osa_5/bloglist-frontend/src/components/Blog.test.js
+++ osa_5/bloglist-frontend/src/components/Blog.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test('render content', () => {
+test('render title/author', () => {
   const newBlog = {
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
+    user: {
+      name: 'Bruce Wayne',
+    },
   };
 
   const component = render(<Blog blog={newBlog} />);
@@ -14,4 +17,58 @@ test('render content', () => {
   expect(component.container).toHaveTextContent(
     'Go To Statement Considered Harmful'
   );
+
+  expect(component.container).toHaveTextContent('Edsger W. Dijkstra');
+
+  expect(component.container).not.toHaveTextContent('www.google.com');
+});
+
+test('render url/likes', () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'www.testing.com',
+    likes: 5,
+    user: {
+      name: 'Bruce Wayne',
+    },
+  };
+
+  const user = {
+    name: 'Bruce Wayne',
+  };
+
+  const component = render(<Blog blog={newBlog} user={user} />);
+  const div = component.container.querySelector('.togglableContent');
+  expect(div).toHaveStyle('display: none');
+
+  const button = component.getByText('show');
+  fireEvent.click(button);
+
+  expect(div).not.toHaveStyle('display: none');
+});
+
+test('like clicked', () => {
+  const newBlog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'www.testing.com',
+    likes: 5,
+    user: {
+      name: 'Bruce Wayne',
+    },
+  };
+
+  const user = {
+    name: 'Bruce Wayne',
+  };
+
+  const mockHandler = jest.fn();
+
+  const component = render(<Blog blog={newBlog} like={mockHandler} />);
+
+  const button = component.getByText('like');
+  fireEvent.click(button);
+
+  expect(mockHandler.mock.calls).toHaveLength(1);
 });
